9d42e91230b101334e3645c2decb8823e4547ad1
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-20 18:17:40 -0500

    Login, signup works

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  devise_for :models
   root to: 'home#index'
+
+  resources :users
 
 end
