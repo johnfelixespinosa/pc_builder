819d182b0d8ddd66430a86ffbab2a61ca1120629
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-20 19:10:03 -0500

    User can create parts list

--- app/controllers/parts_lists_controller.rb
+++ app/controllers/parts_lists_controller.rb
@@ -1,6 +1,26 @@
 class PartsListsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
 
   def index
+  end
+
+  def new
+    @parts_list = PartsList.new
+  end
+
+  def create
+    @parts_list = current_user.parts_lists.build(parts_list_params)
+    if @parts_list.save
+      flash[:success] = "Parts List Added!"
+      redirect_to user_parts_list_path(@parts_list)
+    else
+      flash.now[:message] = "<strong>Please try again. There were some errors:</strong><br>"
+      render :new
+    end
+  end
+
+  def show
   end
 
   private
@@ -18,6 +38,10 @@ class PartsListsController < ApplicationController
         :operating_system, 
         :monitor
       )
+    end
+
+    def find_list
+      @parts_list = PartsList.find_by_id(params[:id])
     end
 
 end
