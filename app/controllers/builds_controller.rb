3cc823b2cece990dbb02eba18905145d58f98241
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-24 14:52:40 -0500

    Stub out builds_controller

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,2 +1,40 @@
 class BuildsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def build_params
+      params.require(:build).permit(
+        :title,
+        :description,
+        images: []
+      )
+    end
+
+    def find_build
+      @build = Build.find_by_id(params[:id])
+    end
+
 end
--- app/models/build.rb
+++ app/models/build.rb
@@ -1,2 +1,7 @@
 class Build < ApplicationRecord
+  has_many_attached :images
+  
+  has_one :parts_list
+  has_one :user, through: :parts_list
+
 end
