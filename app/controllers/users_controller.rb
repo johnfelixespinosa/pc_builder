20d29f00bcc5dc7abfb0fad82e35907e7ba56da2
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-12-20 18:47:50 -0500

    Add Option to delete account on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,11 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    session[:user_id] = nil
+    flash[:success] = "Account successfuly deleted!"
+    redirect_to root_path
+  end
+
 end
