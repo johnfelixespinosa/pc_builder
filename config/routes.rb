59c04e8364ae868a456a3f6b64d9e8d7bbeab29b
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-12-20 18:29:03 -0500

    Configure Omniauth to work with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   root to: 'home#index'
 
   resources :parts_lists, except: [:show]
