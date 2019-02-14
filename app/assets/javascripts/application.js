5f31f5248eca03f5ebc07a23eb30044fd1cd27ab
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-02-14 18:52:33 -0500

    Fixed js only working on page reload, turbolinks issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require jquery
+//= require activestorage
 //= require turbolinks
 //= require popper
 //= require bootstrap-sprockets
