6962b5ad0bc3411c02014491fdcb02e17c990ad1
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-02-16 21:13:53 -0500

    Partial comments from collection, Comments JS still broken

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -6,10 +6,38 @@ $(window).bind('turbolinks:load', function() {
 });
 function initPage() {
 
+  var Comment = function(body, created_at){
+    this.body = body;
+    this.created_at = created_at.slice(0,10);
+    this.user_id = user_id
+  };
+
   $(function(){
-    $('.new_comment').on('submit', function(e){
-      e.preventDefault();
-      console.log("stopped default")
-    })
-  })
+    $("form").submit(function(event) {
+      event.preventDefault();
+      var values = $(this).serialize();
+      var url = $(this).attr("action");
+
+      console.log(url);
+      console.log(values)
+
+      var posting = $.post( url, values);
+      posting.done(function(data){
+        console.log(data.created_at);
+        comment = new Comment(data.body, data.created_at, data.user_id);
+        comment.addComment();
+      });
+
+      console.log(url);
+      console.log(values)
+      
+      
+
+      // createComment(url, values);
+    });
+  });
+
+
+
+  
 }
