8ba8e33cbee53a9afa91df141a8c7dd8884653e1
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-02-24 14:35:35 -0500

    Template the parts_lists and use class construction to create new handlebars templates, need to make layout more intuitive, but parts_list construction works

--- app/assets/javascripts/parts_list.js
+++ app/assets/javascripts/parts_list.js
@@ -1,4 +1,20 @@
 console.log("parts_list.js loaded")
+class partsList{
+  constructor(attributes){
+  this.name = attributes.name;
+  this.cpu = attributes.cpu;
+  this.cpu_cooler = attributes.cpu_cooler;
+  this.motherboard = attributes.motherboard;
+  this.storage = attributes.storage;
+  this.video_card = attributes.video_card;
+  this.monitor = attributes.monitor;
+  this.power_supply = attributes.power_supply;
+  this.operating_system = attributes.operating_system;
+  this.id = attributes.id;
+  this.case = attributes.case;
+  this.user = attributes.user.username;
+  }
+}
 
 $(function(){ 
   var uid = parseInt($('#user').attr("data-user_id"))
@@ -8,13 +24,34 @@ $(function(){
   console.log("this:", this)
   console.log("data:", data)
 
-  for(var i = 0; i < data.length; i++) {
-    var d = data[i];
-    var url_id = parseInt($('#user').attr("data-user_id"))
-    var url = url_id + '/parts_lists/' + d.id
-    console.log('id: ' + url_id + ' name: ' + d.name + ' url: ' + url );
-    $(".parts-lists").append("<li class='list-group-item'><a href='"+ url +"'>"+d.name+"</a></li>")
-  };
+  data.forEach(function(parts_list) {
+    var new_pl = new partsList(parts_list);
+    var new_pl_string = HandlebarsTemplates['parts_lists/parts_list'](new_pl)
+    $(".parts-lists").append(new_pl_string)
+  });
+
+  // $.post(action, params)
+  // .success(function(json){
+  // console.log("json:", json)
+  
+  // var parts_list = new partsList(json)
+  // var new_comment_string = HandlebarsTemplates['comments/comment'](comment)
+  // console.log("comment:", comment)
+  // console.log("new_comment_string:", new_comment_string)
+
+
+
+
+
+
+
+  // for(var i = 0; i < data.length; i++) {
+  //   var d = data[i];
+  //   var url_id = parseInt($('#user').attr("data-user_id"))
+  //   var url = url_id + '/parts_lists/' + d.id
+  //   console.log('id: ' + url_id + ' name: ' + d.name + ' url: ' + url );
+  //   $(".parts-lists").append("<li class='list-group-item'><a href='"+ url +"'>"+d.name+"</a></li>")
+  // };
 
   });
 });
