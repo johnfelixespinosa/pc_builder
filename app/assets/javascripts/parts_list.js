ee7ac2f710a6c8ac0cb87753185169f66a4377a3
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-03-01 14:39:28 -0500

    Refactor parts-list.js sort feature

--- app/assets/javascripts/parts_list.js
+++ app/assets/javascripts/parts_list.js
@@ -1,56 +1,54 @@
-$(".parts_lists").ready(function() {
-  console.log("parts_list.js loaded")
-  class partsList{
-    constructor(attributes){
-    this.name = attributes.name;
-    this.cpu = attributes.cpu;
-    this.cpu_cooler = attributes.cpu_cooler;
-    this.motherboard = attributes.motherboard;
-    this.storage = attributes.storage;
-    this.video_card = attributes.video_card;
-    this.monitor = attributes.monitor;
-    this.power_supply = attributes.power_supply;
-    this.operating_system = attributes.operating_system;
-    this.id = attributes.id;
-    this.case = attributes.case;
-    this.user = attributes.user.username;
-    this.userId = attributes.user.id;
-    this.created_at = attributes.created_at.slice(0,10);
-    }
-  }
-
-  partsList.prototype.toString = function() {
-    return HandlebarsTemplates['parts_lists/parts_list'](this)
+console.log("parts_list.js loaded")
+class partsList{
+  constructor(attributes){
+  this.name = attributes.name;
+  this.cpu = attributes.cpu;
+  this.cpu_cooler = attributes.cpu_cooler;
+  this.motherboard = attributes.motherboard;
+  this.storage = attributes.storage;
+  this.video_card = attributes.video_card;
+  this.monitor = attributes.monitor;
+  this.power_supply = attributes.power_supply;
+  this.operating_system = attributes.operating_system;
+  this.id = attributes.id;
+  this.case = attributes.case;
+  this.user = attributes.user.username;
+  this.userId = attributes.user.id;
+  this.created_at = attributes.created_at.slice(0,10);
   };
+};
 
-  $(function(){ 
-    var uid = parseInt($('#user').attr("data-user_id"))
-    console.log("userId:", uid)
-    getData();
-    
-    function getData(){
-      $.get("/users/" + uid + "/parts_lists.json", data => {
-        console.log("data:", data)
-        jsonData = data
-        makeLis(jsonData);
-      });
-    }  
+partsList.prototype.toString = function() {
+  return HandlebarsTemplates['parts_lists/parts_list'](this)
+};
 
-    function makeLis(jsonData) {
-      jsonData.forEach( parts_list => {
-        var new_pl = new partsList(parts_list);
-        var new_pl_string = new_pl.toString()
-        $(".parts-lists").append(new_pl_string)
-      });
-    }
-
-    $(".js-sort").on('click', function(){
-      getData()
-      jsonData.sort((b,a) => (a.cpu < b.cpu) ? 1 : ((b.cpu < a.cpu) ? -1 : 0));
-      console.log("jsonData :", jsonData)
-      $(".parts-lists").empty()
+$(function(){ 
+  const uid = parseInt($('#user').attr("data-user_id"))
+  console.log("userId:", uid)
+  
+  let jsonData = []
+  getData();
+  
+  function getData(){
+    $.get("/users/" + uid + "/parts_lists.json", data => {
+      jsonData = data
       makeLis(jsonData);
     });
+  }  
 
+  function makeLis(jsonData) {
+    $(".parts-lists").empty()
+    jsonData.forEach( parts_list => {
+      var new_pl = new partsList(parts_list);
+      var new_pl_string = new_pl.toString()
+      $(".parts-lists").append(new_pl_string)
+    });
+  }
+
+  $(".js-sort").on('click', function(){
+    jsonData.sort((b,a) => (a.cpu < b.cpu) ? 1 : ((b.cpu < a.cpu) ? -1 : 0));
+    console.log("jsonData :", jsonData)
+    makeLis(jsonData);
   });
+
 });
